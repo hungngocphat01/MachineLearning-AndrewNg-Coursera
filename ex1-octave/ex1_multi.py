a83258af6802ffc60e47089fb95aa691fd803cc7
hungngocphat01 <hungngocphat01@gmail.com>
2021-07-16 21:37:00 +0700

    Python implementation corrected

--- ex1-octave/ex1_multi.py
+++ ex1-octave/ex1_multi.py
@@ -28,14 +28,12 @@ def gradientDescent(X, Y, theta, alpha, num_iter):
     J_history = [0 for i in range(num_iter)]
     m = len(Y)
 
-    for i in range(num_iter):
-        # Errors
-        e = np.matmul(X, theta) - Y
-        # Change in theta
-        delta = 1/m * np.sum(np.multiply(e.reshape(-1, 1), X), axis=0)
-        theta = theta - alpha * delta
+    for k in range(num_iter):
+        e = np.array(X @ theta - Y, dtype="double")
+        delta = np.array(1/m * np.sum(np.multiply(e.reshape(-1, 1), X), axis=0), dtype="double")
+        theta -= alpha * delta
 
-        print(theta, computeCostJ(X, Y, theta))
+        J_history[k] = computeCostJ(X, Y, theta)
     
     return theta, J_history
 
@@ -43,16 +41,15 @@ def gradientDescent(X, Y, theta, alpha, num_iter):
 # Read data from CSV 
 df = pd.read_csv("ex1data2.txt", header=None)
 
-X = np.array(df[[0, 1]]).astype(float)
-Y = np.array(df[2]).astype(float)
+X = np.array(df[[0, 1]], dtype="double")
+Y = np.array(df[2], dtype="double")
 
 # Normalize data
-X, mu, sigma = featureNormalize(X)
+# X, mu, sigma = featureNormalize(X)
 X = np.hstack((np.ones((X.shape[0], 1)), X))
 
 # Run gradient descent 
-theta = [0, 0, 0]
+theta = np.array([0, 0, 0], dtype="double")
 alpha = 0.0000001
 num_iter = 400
 theta, J_history = gradientDescent(X, Y, theta, alpha, num_iter)
-
