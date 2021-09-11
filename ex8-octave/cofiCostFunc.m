3a908494cc9615951b886cc1f2e254abb1aa4832
hungngocphat01 <hungngocphat01@gmail.com>
2021-09-11 21:23:24 +0700

    Add Recommender Sys

--- ex8-octave/cofiCostFunc.m
+++ ex8-octave/cofiCostFunc.m
@@ -41,18 +41,9 @@ Theta_grad = zeros(size(Theta));
 %
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+J = 0.5 * sum(sum((((X * Theta') - Y).*R).^2)) + 0.5 * lambda * (sum(sum(Theta.^2)) + sum(sum(X.^2)));
+X_grad = ((X * Theta' - Y) .* R) * Theta + lambda * X;
+Theta_grad = ((X * Theta' - Y) .* R)' * X + lambda * Theta;
 
 
 % =============================================================
